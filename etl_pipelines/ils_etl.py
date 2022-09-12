e5f3ea8a5f36e3a3fa4689a7cd180e285556e8f7
Michael M <melancholic_maclean@yahoo.com>
2022-09-12 00:38:55 -0400

    testing pipelines

--- etl_pipelines/ils_etl.py
+++ etl_pipelines/ils_etl.py
@@ -55,20 +55,54 @@ def clean_dataframe(df, h3_resolution=8):
     return df
 
 
+def chunks(lst, n):
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+
 def load(df):
     server = IowaData.azure_server
     user = IowaData.azure_uid
     password = IowaData.azure_pwd
 
-    insert_query = f"""INSERT INTO invoices.temptable({', '.join([*df.columns])}) 
-    VALUES ({', '.join(['%s' for col in df.columns])})"""
-
-    write_data = tuple(map(tuple, df.fillna(0).values))
+    write_data = list(map(tuple, df.fillna(0).values))
+    insert_query = f"""INSERT INTO invoices.temptable({', '.join([*df.columns])}) VALUES ({', '.join(['%s' for col in df.columns])})"""
+    insert_q, values_q = insert_query.split('VALUES') # get part with the query and the parameters
+    insert_q += 'VALUES' # add values to make sql query correct after split
 
     with pymssql.connect(server, user, password, "iowa_liquor") as cnxn:
         with cnxn.cursor() as cur:
-            cur.executemany(insert_query, write_data)
-            cnxn.commit()
+
+            for chunk_data in chunks(write_data, 1000):
+                # chunk_data contains list of row parameters
+                flat_list = [item for sublist in chunk_data for item in sublist] # we make it flat to use execute later instead execute_many
+                chunk_query = insert_q + ','.join([values_q] * len(chunk_data)) # creating the query with multiple values insert
+                cur.execute(chunk_query, tuple(flat_list))
+                cnxn.commit()
+
+
+
+    # write_data = [str(rec) for rec in df.fillna(0).to_records(index=False)]
+    # insert_query_part = f"INSERT INTO invoices.temptable({', '.join([*df.columns])})"
+    #
+    # for chunk in tqdm(chunks(write_data, 10000)):
+    #     val_query_part = f"VALUES {', '.join(chunk)}"
+    #     insert_chunk_query = insert_query_part + "\n" + val_query_part + ";"
+    #     with pymssql.connect(server, user, password, "iowa_liquor") as cnxn:
+    #         with cnxn.cursor() as cur:
+    #             cur.execute(insert_chunk_query, write_data)
+    #             cnxn.commit()
+
+
+    # insert_query = f"""INSERT INTO invoices.temptable({', '.join([*df.columns])})
+    # VALUES ({', '.join(['%s' for col in df.columns])})"""
+    #
+    # write_data = tuple(map(tuple, df.fillna(0).values))
+    #
+    # with pymssql.connect(server, user, password, "iowa_liquor") as cnxn:
+    #     with cnxn.cursor() as cur:
+    #         cur.executemany(insert_query, write_data)
+    #         cnxn.commit()
 
 
 def main():
@@ -88,9 +122,12 @@ def main():
         run_start_time = datetime.now()
         try:
             date_between = f"(date between '{interval[0]}' and '{interval[1]}')"
+            print(f"Retrieving: {interval}")
             results = client.get_all(liquor_sales_data_id, where=date_between)
             socrata_df = pd.DataFrame.from_records(results)
             socrata_df = clean_dataframe(socrata_df)
+            print(f"Max date: {socrata_df['date'].max()}")
+            print("Loading results...")
             load(socrata_df)
             log(run_start_time, interval)
 
